6fd40e50c7d9d6eca05dd0931ebce65a1cea1a91
Clément MOURGUE <cmourgue@iutbayonne.univ-pau.fr>
2023-11-21 13:47:52 +0100

    Finalisation du code d'association des mots aux tags grâce au dicoSynTag avec des enrichissements de différents degrés et de différentes sources

--- src/dicoSynTag/Codes/motToTag.py
+++ src/dicoSynTag/Codes/motToTag.py
@@ -40,7 +40,7 @@ listeMot = ['female parent','get']
 """
  * @brief Le programme principal créant le dictionnaire dicoSynTag
  * 
- * @return int = 0
+ * @return tuple (dicoMotToTag,listeTag)
  """
 def main():
 
@@ -50,12 +50,35 @@ def main():
 
     # TRAITEMENTS
     for motCourant in listeMot :
-        listeSynMot = synAvecAPI(motCourant)['synonyms']
-        for synMotCourant in listeSynMot :
-            if synMotCourant in dicoSynTag :
-                
+        if motCourant in dicoMotToTag.keys() : # Vérif mot en double
+            print(motCourant + " déjà présent dans le dicoMotToTag")
+            pass
+        # Vérif présence dans dicoSynTag du mot
+        if motCourant in dicoSynTag.keys() : # Si présence ajout et enregistrement
+            dicoMotToTag[motCourant] = [dicoSynTag[motCourant]]
+            listeTag.append(dicoSynTag[motCourant])
+        else : # Sinon enrichissement de 1 degré à partir des mots
+            listeSynMot = synAvecAPI(motCourant)['synonyms']
+            for synMotCourant in listeSynMot :
+                if synMotCourant in dicoSynTag.keys() : # Si présence ajout et enregistrement
+                    dicoMotToTag[motCourant] = [synMotCourant,dicoSynTag[motCourant]]
+                    listeTag.append(dicoSynTag[motCourant])
+                else : # Sinon enrichissement de 1 degré supplémentaire (degré 2) à partir des tags
+                    for syn in dicoSynTag.keys() :
+                        listeSynSynDicoSynTag = synAvecAPI(syn)['synonyms']
+                        if motCourant in listeSynSynDicoSynTag : # Vérif présence du mot de base dans les synonymes de synonymes de tag
+                            dicoMotToTag[motCourant] = [syn,dicoSynTag[motCourant]]
+                            listeTag.append(dicoSynTag[motCourant])
+                        elif synMotCourant in listeSynSynDicoSynTag : # Vérif présence du synonyme du mot dans les synonymes de synonymes de tag
+                            dicoMotToTag[motCourant] = [synMotCourant,syn,dicoSynTag[motCourant]]
+                            listeTag.append(dicoSynTag[motCourant])
+                        else : # Pas de liaison avec le corpus de tags
+                            dicoMotToTag[motCourant] = ['Impossible à lier']
     
-    return 0
+    print(dicoMotToTag)
+    print(listeTag)
+    
+    return (dicoMotToTag,listeTag)
 
 
 # DÉFINITION DES SOUS-PROGRAMMES
