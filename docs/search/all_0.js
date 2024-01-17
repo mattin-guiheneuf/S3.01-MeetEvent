ac1698cd513f3aa4b9ca226761bb5c7e4c35baf5
Mattin Guiheneuf <mguiheneuf@iutbayonne.univ-pau.fr>
2024-01-17 12:31:21 +0100

    Modif du doxyfile

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_5f_5fconstruct_0',['__construct',['../class_corpus.html#a504bc409073a1191a1e3a3f9c2edfc8b',1,'Corpus\__construct()'],['../class_evenement.html#a267ba4bd5b4727a3850edc652c63496d',1,'Evenement\__construct()'],['../class_tag.html#a7c023ca949b3bc93f653c8ae9e096a35',1,'Tag\__construct()'],['../class_utilisateur.html#ac4b6260e74b0356f0bd113ca8833b6ba',1,'Utilisateur\__construct()']]]
+  ['_5f_5fconstruct_0',['__construct',['../class_corpus.html#a504bc409073a1191a1e3a3f9c2edfc8b',1,'Corpus\__construct()'],['../class_dico_syn_tags.html#af2afb599013d19a1523fdf5f748062ba',1,'DicoSynTags\__construct()'],['../class_evenement.html#a267ba4bd5b4727a3850edc652c63496d',1,'Evenement\__construct()'],['../class_tag.html#a7c023ca949b3bc93f653c8ae9e096a35',1,'Tag\__construct()'],['../class_utilisateur.html#ac4b6260e74b0356f0bd113ca8833b6ba',1,'Utilisateur\__construct()']]]
 ];
--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['definirdescription_0',['definirDescription',['../class_evenement.html#aff2b3d4efed099deea9be0462fb0b18e',1,'Evenement\definirDescription()'],['../class_utilisateur.html#a78378d5a350b7fecfc36d36b174672c4',1,'Utilisateur\definirDescription()']]],
-  ['dycosyntags_1',['DycoSynTags',['../class_dyco_syn_tags.html',1,'']]]
+  ['definirtags_1',['definirTags',['../class_evenement.html#a57975e113de48b18a7d393bbba3f7ce6',1,'Evenement\definirTags()'],['../class_utilisateur.html#accabe5bd2bdd44ad292ee46c81ddcc39',1,'Utilisateur\definirTags()']]],
+  ['determinersynonymes_2',['determinerSynonymes',['../class_dico_syn_tags.html#a40e867f4f47567387a95d50a63428112',1,'DicoSynTags']]],
+  ['dicosyntags_3',['DicoSynTags',['../class_dico_syn_tags.html',1,'']]]
 ];
